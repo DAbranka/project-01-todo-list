cc0a26e3649aa524799be1d7f419a238a1ea2e6a
DAbranka <d.abranka@hotmail.com>
2025-11-22 16:58:20 +0100

    Refactor TodoList component to properly append todos to the DOM

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,24 @@
+import { createElement } from "../functions/dom";
+
 export class TodoList {
-    
+
     #todos
-    
+
     constructor(todos) {
         this.#todos = todos;
     }
 
+    get todos() {
+        return this.#todos
+    }
+
     appendTo(element) {
-        element = `Hello World`
+        this.#todos.forEach(e => {
+            const li = createElement('li', {
+                class: 'todo-title',
+            })
+            li.innerText = e.title
+            element.append(li)
+        });
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import { fetchJSON } from "./functions/api.js";
 import { TodoList } from "./components/TodoList.js";
-import {createElement} from "./functions/dom.js"
+import { createElement } from "./functions/dom.js"
 
 const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos/?_limit=5');
 console.log(todos)
 
+// * NEW LIST
 const ul = document.querySelector('.todosList ul')
-console.log(ul);
-
-todos.forEach((e) => {
-  const li = createElement('li', {
-    class: 'todo-title'
-  })
-  li.innerText = e.title
-  ul.append(li)
-})
+const list = new TodoList(todos)
+list.appendTo(ul)
