b826a1d74ae31f9e911bfbc477dec402193449eb
DAbranka <d.abranka@hotmail.com>
2025-11-23 15:41:02 +0100

    Update title and header in index.html; enhance form validation in TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,7 @@ import { createElement } from "../functions/dom";
 export class TodoList {
 
     #todos = []
+    #list
 
     constructor(todos) {
         this.#todos = todos;
@@ -13,10 +14,37 @@ export class TodoList {
     }
 
     appendTo(element) {
+        this.#list = document.querySelector('.todosList ul')
         this.#todos.forEach(e => {
             const todo = new TodoListItem(e)
             element.append(todo.element)
         });
+
+        document.querySelector('.add-task-form').addEventListener('submit', (e) => {
+            this.onSubmit(e)
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        const form = e.currentTarget
+        const title = new FormData(form)
+            .get('todoTitle')
+            .toString()
+            .trim()
+        if (title === '') {
+            return
+        }
+        const newTodo = {
+            id: Date.now(),
+            title,
+            completed: false
+        }
+        const todoItem = new TodoListItem(newTodo)
+
+        this.#list.prepend(todoItem.element)
+        console.log('Todos List', this.#list);
+        form.reset()
     }
 }
 
@@ -40,7 +68,7 @@ class TodoListItem {
         const checkbox = createElement('input', {
             type: 'checkbox',
             id,
-            checked: todo.completed || false
+            checked: todo.completed ? '' : null
         })
         checkbox.addEventListener('change', () => {
             if (checkbox.checked) {
@@ -58,7 +86,7 @@ class TodoListItem {
             for: id
         })
         label.innerText = todo.title
-        
+
         labelContainer.append(label)
 
         /* -------------------------------*/
